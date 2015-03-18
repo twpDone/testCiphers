097b301348873f2791fb3d8a9a24be3bebd60613
root <root@localhost.localdomain>
2015-03-18 18:03:28 +0100

    Factorisation du code; usage message; gestion des argument; message d erreur pour ssl2

--- testCiphers.sh
+++ testCiphers.sh
@@ -1,9 +1,60 @@
 #!/bin/bash
 
+usage(){
+    echo ""
+    echo -n "Usage :"$0
+    tput setf 1 ; echo -n " IP:port" ;
+    tput setf 6 ; echo " [protocol]";
+    tput setf 9; 
+    echo ""
+    echo -n "Values for the "; tput setf 6; echo -n "protocol"; tput setf 9; echo " option are :"
+    echo "-tls (TLS Only)" 
+    echo "-ssl (SSL ONLY)" 
+    echo ""
+    echo -n "If "; tput setf 6; echo -n "protocol"; tput setf 9; echo " is absent both (tls and ssl) will be checked"
+    echo ""
+}
+
+if test $# -eq 0
+then
+    usage
+    exit 1
+else
+    if test "$1" = "-h"
+    then
+        usage
+        exit 1
+    fi
+    if test "$1" = "--help"
+    then
+        usage
+        exit 1
+    fi
+    case $1 in
+        *:*) ;; # correct syntax IP:port
+        *) echo ""; tput setf 4 ; echo "Incorrect syntax for arg1"; tput setf 9 ; usage; exit 1 ;;
+    esac
+fi
+
 ipPort=$1
+protocol="ALL"
+
+case "$2" in
+    "-tls") protocol="tlsOnly" ;;
+    "-ssl") protocol="sslOnly" ;;
+    "") ;;
+    *) echo "Wrong value for protocol option"
+        usage 
+        exit 1
+        ;;
+esac
+
+echo "[set] -protocol "$protocol
+echo ""
+    
 
 #               sslv2 sslv3 tlsv1 tlsv1.1 tlsv1.2
-SSL_VERSIONS="-ssl2 -ssl3 -tls1 --no_tls1_2 ''"
+#SSL_VERSIONS="-ssl2 -ssl3 -tls1 --no_tls1_2 ''"
 AVAILIABLE_CIPHERS=`openssl ciphers | sed s/':'/' '/g`
 
 cipher(){
@@ -16,7 +67,6 @@ cipher(){
 
     if test -n "$sslversion"
     then
-        
         sslversionOpt=$sslversion
         #[-ssl2] [-ssl3] [-tls1] [-no_ssl2] [-no_ssl3] [-no_tls1] [-no_tls1_1] [-no_tls1_2]
     fi
@@ -26,7 +76,7 @@ cipher(){
         cipherOpt="-cipher "$cipher
     fi
 
-    result=$(echo "" | openssl s_client $sslversion $cipherOpt -connect $ipPort 2>/dev/null)
+    result=$(echo "" | openssl s_client $sslversion $cipherOpt -connect "$ipPort" 2>/dev/null)
     if test $? -eq 0 
     then
         algo=`echo $result | egrep "Cipher\s*:" | sed -e s/".*\(Cipher\s* : [a-zA-Z0-9\-]* \).*"/"\1"/`
@@ -34,58 +84,91 @@ cipher(){
         echo "Accepting "$proto", "$algo
     fi
 }
-testSSL2(){
+
+checkCipherFromList(){
     ipPort=$1
-    echo "Checking SSLv2 support"
-        cipher $ipPort -ssl2 
+    cipherList=$2
+    sslVersionOption=$3
+    for algo in `echo $cipherList`
+    do
+        cipher $ipPort "$sslVersionOption" $algo 
+    done
+}
+
+getSSLVersionOption(){
+    algorithm=$1
+    algoVersion=$2
+    if test "$algorithm" = "ssl"
+    then
+        case "$algoVersion" in
+            "2") #echo "-ssl2"
+                 #exit 0
+                echo "SSLv2 NOT SUPPORTED ! The openssl s_client option is supposed to be -ssl2, but openssl returns 'unknown option -ssl2'"
+                exit 128
+                ;;
+            "3") echo "-ssl3"
+                exit 0
+                ;;
+            *) echo $algorithm"v"$algoVersion" doesn't exist."
+                exit 127
+                ;;
+        esac
+    fi 
+    if test "$algorithm" = "tls"
+    then
+        case "$algoVersion" in
+            "1.0") echo "-tls1"
+                exit 0
+                ;;
+            "1.1") echo "-no_tls1_2"
+                exit 0
+                ;;
+            "1.2") echo ""
+                exit 0
+                ;;
+            *) echo $algorithm"v"$algoVersion" doesn't exist."
+                exit 127
+                ;;
+        esac
+    else
+        echo $algorithm" doesn't exist."
+        exit 127
+    fi 
+
+}
+
+cipherTest(){
+    ipPort=$1
+    algo=$2
+    algoVersion=$3
+    opensslOptionSSLVersion=`getSSLVersionOption "$2" "$3"`
+    ret=$?
+    if test "$ret" -ne 0
+    then
+        echo "Error ("$ret"): " $opensslOptionSSLVersion
+        #exit $ret
+    fi
+    echo "Checking "$algo"v"$algoVersion" support"
+    checkCipherFromList "$ipPort" "$AVAILIABLE_CIPHERS" "$opensslOptionSSLVersion"
     echo""
 }
 
+if test $protocol = "sslOnly" -o $protocol = "ALL"
+then
+    tput setf 4;
+    echo "----"
+    echo "SSLv2 Wont work now due to a handling arguments problem in 'openssl s_client'"
+    cipherTest $ipPort "ssl" 2
+    echo "----"
+    echo ""
+    tput setf 9;
+    cipherTest $ipPort "ssl" 3
+fi
 
-testSSL3(){
-    ipPort=$1
-    echo "Checking SSLv3 support"
-        cipher $ipPort -ssl3 
-    echo""
-}
-
-testTSLv1.0(){
-    ipPort=$1
-    echo "Checking TSLv1.0 support"
-        cipher $ipPort -tls1
-    echo""
-}
-
-testTSLv1.1(){
-    ipPort=$1
-    echo "Checking TSLv1.1 support"
-        cipher $ipPort -no_tls1_2
-    echo""
-}
-
-testTSLv1.2(){
-    ipPort=$1
-    echo "Checking TSLv1.2 support"
-        for algo in `echo $AVAILIABLE_CIPHERS`
-        do
-            #echo $algo
-            cipher $ipPort '' $algo
-        done
-    echo""
-}
-
-testSSL2 $ipPort
-testSSL3 $ipPort
-testTSLv1.0 $ipPort
-testTSLv1.1 $ipPort
-testTSLv1.2 $ipPort
-
-#echo "[-ssl2] [-ssl3] [-tls1] [-no_ssl2] [-no_ssl3] [-no_tls1] [-no_tls1_1] [-no_tls1_2]"
-#read ssl
-#
-#echo ""
-#echo "EXP-RC4-MD5"
-#read mcipher
-#
-#cipher $ipPort $ssl $mcipher
+if test $protocol = "tlsOnly" -o $protocol = "ALL" 
+then
+    cipherTest $ipPort "tls" "1.0"
+    cipherTest $ipPort "tls" "1.1"
+    cipherTest $ipPort "tls" "1.2"
+fi
 
